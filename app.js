91d2be9a70ebf368c4a12c7afcb70c0689b536f6
whisping <whisping@gmail.com>
2019-09-02 21:26:03 +0300

    added possibility of removing items from the list

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useNewUrlParser: true
+});
+mongoose.set('useFindAndModify', false);
 
 const itemsSchema = {
     name: String,
@@ -51,9 +56,12 @@ app.get("/", function(req, res) {
                 }
             })
             res.redirect("/");
-        }   else {
+        } else {
 
-            res.render("list", {listTitle: "Today", newListItems: foundItems});
+            res.render("list", {
+                listTitle: "Today",
+                newListItems: foundItems
+            });
             // console.log(foundItems);
         }
     });
@@ -63,27 +71,44 @@ app.get("/", function(req, res) {
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
-  const item = req.body.newItem;
+    const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+    const item = new Item({
+        name : itemName
+    })
+
+    item.save();
+
     res.redirect("/");
-  }
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function(req, res) {
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if (err) {
+            console.log(err);
+        }   else {
+            console.log("Succesfully remove checked item ");
+            res.redirect("/");
+        }
+    })
+})
+
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
